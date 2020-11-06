48d20beb3f850a85b2eb0698a86eec24a6208857
tedtheripper <lemon.juice0731@gmail.com>
2020-11-06 10:53:40 +0100

    Creating new react js project

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 align='center'>Hello Adrian</h1>
+  );
+}
+
+
+
+export default App; 
+
---
+++ app/src/Present.js
@@ -0,0 +1,8 @@
+
+function Present() {
+    return (
+      <h3>it works</h3>
+    )
+  }
+
+  export default Present;
