7d98af5f2b76d79eaf3b24c0b85f079daf65685c
tedtheripper <lemon.juice0731@gmail.com>
2020-11-06 14:43:58 +0100

    listing example messages

--- my-app/src/LeftSideContent.js
+++ my-app/src/LeftSideContent.js
@@ -1,14 +1,30 @@
 import './LeftSideContent.css';
+import LatestMessage from './latestMessage'
 
-let latestMessages = [];
+function createLatestMessages(){
+    let latestMessages = [];
+    for(let i = 0; i < 10; i++) {
+        let lMessage = new LatestMessage("John", "Hi, I'm John", "2.11.2020");
+        latestMessages[i] = lMessage;
+    }
+    return latestMessages;
+}
 
 function LeftSide() {
-    return ([
-        <div id="menuLeftContent">
-            <h2>Lates messages</h2>
-        </div>,
-        <div></div>
-
-    ])
+    let latMessages = createLatestMessages();
+    let allMessages = [];
+    for(let i = -1; i < latMessages.length; i++) {
+        let item;
+        if(i === -1){
+            item = <div id="menuLeftContent"><h2>Latest messages</h2></div>
+        }
+        else {
+        item = <div id="latestMessage">
+            <p>{latMessages[i].name} | {latMessages[i].message} | {latMessages[i].date}
+            </p></div>
+        }
+        allMessages.push(item)
+    }
+    return(allMessages)
 }
 export default LeftSide;
