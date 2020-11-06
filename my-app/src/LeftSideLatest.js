611b34390b4835f446ec11f5a192c40dbb10c336
tedtheripper <lemon.juice0731@gmail.com>
2020-11-06 17:56:01 +0100

    different font styles for latest messages

--- my-app/src/LeftSideLatest.js
+++ my-app/src/LeftSideLatest.js
@@ -4,7 +4,7 @@ import './LeftSideLatest.css';
 function createLatestMessages(){
     let latestMessages = [];
     for(let i = 0; i < 12; i++) {
-        let lMessage = new LatestMessage("John", "Hi, I'm John", "2.11.2020");
+        let lMessage = new LatestMessage("John", "ICH HEISSE DONALD TRUMP AND IM THE BEST MAN ON THE PLANET", "2.11.2020");
         latestMessages[i] = lMessage;
     }
     return latestMessages;
@@ -15,6 +15,10 @@ function getAllMessages() {
     let allMessages = [];
     for(let i = 0; i < latMessages.length; i++) {
         let item;
+        if(latMessages[i].message.length > 25){
+            let temp = latMessages[i].message.substring(0, 25);
+            latMessages[i].message = temp + "...";
+        }
         item = <div id="latestMessage">
                     <div id="latestMessageName">{latMessages[i].name}</div>
                     <div id="latestMessageDate">{latMessages[i].date}</div>
